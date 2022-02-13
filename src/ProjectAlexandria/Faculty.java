568f0a49c607b7a598682916410c41ba3a764502
Zeltronick <muhailo11111@gmail.com>
2022-02-13 16:07:41 +0200

    wertjk

---
+++ src/ProjectAlexandria/Faculty.java
@@ -0,0 +1,38 @@
+package ProjectAlexandria;
+
+import java.util.Objects;
+
+public class Faculty {
+    private String name;
+
+    public Faculty(String name) {
+        this.name = name;
+    }
+
+    public void copy(Faculty faculty){
+        this.name = faculty.getName();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Faculty faculty = (Faculty) o;
+        return Objects.equals(name, faculty.name);
+    }
+
+    @Override
+    public String toString() {
+        return "Faculty{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/ProjectAlexandria/Teacher.java
@@ -0,0 +1,9 @@
+package ProjectAlexandria;
+
+public class Teacher extends Human {
+
+    public Teacher(String[] name, Cathedra cathedra) {
+        super(true, name, cathedra);
+    }
+
+}
