519d3951a1662198d249eb97e3bae5090d353f70
Zeltronick <muhailo11111@gmail.com>
2022-03-29 17:52:00 +0300

    Complete_University

--- src/UniversityUtilits/DataInput.java
+++ src/UniversityUtilits/DataInput.java
@@ -10,6 +10,12 @@ public final class DataInput {
 
     private static String error = "Incorrect input value!";
 
+    /**
+     * asks to write byte value
+     *
+     * @return byte value
+     */
+
     public static Byte getByte() {
         boolean isByte = false;
         Byte value = null;
@@ -24,6 +30,13 @@ public final class DataInput {
         } while(!isByte);
         return value;
     }
+
+    /**
+     * asks to write byte value
+     *
+     * @param announcement string which will be written before
+     * @return byte value
+     */
 
     public static Byte getByte(String announcement) {
         boolean isByte = false;
@@ -40,6 +53,12 @@ public final class DataInput {
         return value;
     }
 
+    /**
+     * asks to write short value
+     *
+     * @return short
+     */
+
     public static Short getShort() {
         boolean isShort = false;
         Short value = null;
@@ -54,6 +73,13 @@ public final class DataInput {
         } while(!isShort);
         return value;
     }
+
+    /**
+     * asks to write short value
+     *
+     * @param announcement string which will be written before
+     * @return short value
+     */
 
     public static Short getShort(String announcement) {
         boolean isShort = false;
@@ -70,6 +96,12 @@ public final class DataInput {
         return value;
     }
 
+    /**
+     * asks to write integer value
+     *
+     * @return integer value
+     */
+
     public static Integer getInt(){
         boolean isInt = false;
         Integer value = null;
@@ -84,6 +116,13 @@ public final class DataInput {
         } while(!isInt);
         return value;
     }
+
+    /**
+     * asks to write integer value
+     *
+     * @param announcement string which will be written before
+     * @return integer value
+     */
 
     public static Integer getInt(String announcement){
         boolean isInt = false;
@@ -100,6 +139,12 @@ public final class DataInput {
         return value;
     }
 
+    /**
+     * asks to write long value
+     *
+     * @return long value
+     */
+
     public static Long getLong() {
         boolean isLong = false;
         Long value = null;
@@ -114,6 +159,13 @@ public final class DataInput {
         } while(!isLong);
         return value;
     }
+
+    /**
+     * asks to write long value
+     *
+     * @param announcement string which will be written before
+     * @return long value
+     */
 
     public static Long getLong(String announcement) {
         boolean isLong = false;
@@ -130,6 +182,12 @@ public final class DataInput {
         return value;
     }
 
+    /**
+     * asks to write float value
+     *
+     * @return float value
+     */
+
     public static Float getFloat() {
         boolean isFloat = false;
         Float value = null;
@@ -144,6 +202,13 @@ public final class DataInput {
         } while(!isFloat);
         return value;
     }
+
+    /**
+     * asks to write float value
+     *
+     * @param announcement string which will be written before
+     * @return float value
+     */
 
     public static Float getFloat(String announcement) {
         boolean isFloat = false;
@@ -160,6 +225,12 @@ public final class DataInput {
         return value;
     }
 
+    /**
+     * asks to write double value
+     *
+     * @return double value
+     */
+
     public static Double getDouble(){
         boolean isDouble = false;
         Double value = null;
@@ -174,6 +245,13 @@ public final class DataInput {
         } while(!isDouble);
         return value;
     }
+
+    /**
+     * asks to write double value
+     *
+     * @param announcement string which will be written before
+     * @return double value
+     */
 
     public static Double getDouble(String announcement){
         boolean isDouble = false;
@@ -190,6 +268,12 @@ public final class DataInput {
         return value;
     }
 
+    /**
+     * asks to write char value
+     *
+     * @return char value
+     */
+
     public static char getChar() {
         String s;
         do {
@@ -199,6 +283,13 @@ public final class DataInput {
         return s.charAt(0);
     }
 
+    /**
+     * asks to write char value
+     *
+     * @param announcement string which will be written before
+     * @return char value
+     */
+
     public static char getChar(String announcement) {
         String s;
         do {
@@ -207,6 +298,12 @@ public final class DataInput {
         } while(s.length() != 1);
         return s.charAt(0);
     }
+
+    /**
+     * asks to write string value
+     *
+     * @return string value
+     */
 
     public static String getString(){
         String res = "";
@@ -224,6 +321,13 @@ public final class DataInput {
         return res;
     }
 
+    /**
+     * asks to write string value
+     *
+     * @param announcement string which will be written before
+     * @return string value
+     */
+
     public static String getString(String announcement){
         String res = "";
         boolean isString = false;
@@ -240,6 +344,12 @@ public final class DataInput {
         } while(!isString);
         return res;
     }
+
+    /**
+     * asks to write word value
+     *
+     * @return word value
+     */
 
     public static String getWord() {
         String res = "";
@@ -259,6 +369,13 @@ public final class DataInput {
         return res;
     }
 
+    /**
+     * asks to write word value
+     *
+     * @param announcement string which will be written before
+     * @return word value
+     */
+
     public static String getWord(String announcement) {
         String res = "";
         boolean isWord = false;
@@ -276,6 +393,12 @@ public final class DataInput {
         } while(!isWord);
         return res;
     }
+
+    /**
+     * asks to write sentence value
+     *
+     * @return sentence value
+     */
 
     public static String getSentence() {
         StringBuilder res = new StringBuilder();
@@ -308,6 +431,13 @@ public final class DataInput {
         return res.toString();
     }
 
+    /**
+     * asks to write sentence value
+     *
+     * @param announcement string which will be written before
+     * @return sentence value
+     */
+
     public static String getSentence(String announcement) {
         StringBuilder res = new StringBuilder();
         boolean isSentence = true;
@@ -339,6 +469,12 @@ public final class DataInput {
         return res.toString();
     }
 
+    /**
+     *
+     * @param s string
+     * @return is string a word
+     */
+
     private static boolean isWord(String s) {
         if(s == null)return false;
         if(s.length() <= 0)return false;
@@ -347,6 +483,13 @@ public final class DataInput {
         }
         return true;
     }
+
+    /**
+     *
+     * @param trueValue true value
+     * @param falseValue false value
+     * @return boolean value
+     */
 
     public static Boolean getBoolean(String trueValue, String falseValue){
         Boolean value = null;
@@ -365,6 +508,14 @@ public final class DataInput {
         return value;
     }
 
+    /**
+     *
+     * @param announcement string which will be written before
+     * @param trueValue true value
+     * @param falseValue false value
+     * @return boolean value
+     */
+
     public static Boolean getBoolean(String announcement, String trueValue, String falseValue){
         Boolean value = null;
         do {
@@ -382,9 +533,21 @@ public final class DataInput {
         return value;
     }
 
+    /**
+     *
+     *
+     * @return error string
+     */
+
     public static String getError() {
         return error;
     }
+
+    /**
+     * sets new error
+     *
+     * @param error new error
+     */
 
     public static void setError(String error) {
         DataInput.error = error;
