e87cc34a270e8cc325588facf73d2736f4ffe740
Zeltronick <muhailo11111@gmail.com>
2022-03-29 16:55:05 +0300

    Complete_University

--- src/UniversityUtilits/Cathedra.java
+++ src/UniversityUtilits/Cathedra.java
@@ -8,6 +8,13 @@ public class Cathedra {
     private String name;
     private Faculty faculty;
 
+    /**
+     * constuctor
+     *
+     * @param name name of the cathedra
+     * @param faculty faculty of the cathedra
+     */
+
     public Cathedra(String name, Faculty faculty) {
         if(name != null) {
             name = name.toLowerCase();
@@ -16,10 +23,22 @@ public class Cathedra {
         this.faculty = faculty;
     }
 
+    /**
+     * copies information from cathedra to this
+     *
+     * @param cathedra specific cathedra
+     */
+
     public void copy(Cathedra cathedra) {
         this.name = cathedra.getName();
         this.faculty = cathedra.getFaculty();
     }
+
+    /**
+     *
+     * @param o specific object
+     * @return are this and o the same or consist the same information
+     */
 
     @Override
     public boolean equals(Object o) {
@@ -29,21 +48,48 @@ public class Cathedra {
         return Objects.equals(name, cathedra.name) && Objects.equals(faculty, cathedra.faculty);
     }
 
+    /**
+     *
+     * @return name of this cathedra
+     */
+
     public String getName() {
         return name;
     }
+
+    /**
+     * sets new name for this cathedra
+     *
+     * @param name new name
+     */
 
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     *
+     * @return faculty of this cathedra
+     */
+
     public Faculty getFaculty() {
         return faculty;
     }
 
+    /**
+     * sets new faculty for this cathedra
+     *
+     * @param faculty new faculty
+     */
+
     public void setFaculty(Faculty faculty) {
         this.faculty = faculty;
     }
+
+    /**
+     *
+     * @return information about this cathedra
+     */
 
     @Override
     public String toString() {
--- src/UniversityUtilits/Teacher.java
+++ src/UniversityUtilits/Teacher.java
@@ -2,9 +2,21 @@ package UniversityUtilits;
 
 public class Teacher extends Human {
 
+    /**
+     * constuctor
+     *
+     * @param name name of the human
+     * @param cathedra specific cathedra
+     */
+
     public Teacher(String[] name, Cathedra cathedra) {
         super(true, name, cathedra);
     }
+
+    /**
+     *
+     * @return information about this teacher
+     */
 
     @Override
     public String toString() {
